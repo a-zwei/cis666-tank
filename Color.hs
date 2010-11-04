14c7d405f0615086d175a76aae80980d753483c0
Fred Maurer <fred@colinux.(none)>
2010-11-04 09:48:54 -0400

    switch to fgl, better names, added 'randomGraph'

--- Color.hs
+++ Color.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.Array.IArray
 import Data.Function (on)
-import Data.Graph
+import Data.Graph.Inductive
 import Data.List (sortBy)
 import System.Random
 
@@ -15,48 +15,52 @@ randomArray bounds range seed = listArray bounds $ randomRs range g
 
 sampleA = State $ randomArray ((1,1),(6,3)) (-0.5, 0.5) 1
 
+type UGraph = Gr () ()
+
+randomGraph :: Int -> Int -> Int -> UGraph
+randomGraph nodes edges seed = undir $ mkUGraph [1..nodes] $ take edges rEdges
+  where rEdges = filter (\(i,j) -> i /= j) $ zip rNodes (tail rNodes)
+        rNodes = randomRs (1, nodes) (mkStdGen seed)
+
 boolToInt True = 1
 boolToInt False = 0
 
 vs :: State -> Array (Int, Int) Int
 vs s = amap (boolToInt . (>= 0)) (us s)
 
-connected :: Graph -> Vertex -> Vertex -> Bool
-connected _ i j | i == j = False
-connected g i j = ((i, j) `elem` gvs) || ((j, i) `elem` gvs)
-  where gvs = edges g
+cv :: UGraph -> Node -> Node -> Int
+cv g i j = boolToInt $ j `elem` neighbors g i
 
-cv :: Graph -> Vertex -> Vertex -> Int
-cv g i j = boolToInt $ connected g i j
+nodeBounds :: State -> (Int, Int)
+nodeBounds s = (\((i,_),(j,_)) -> (i, j)) $ bounds (us s)
 
-rowBounds :: State -> (Int, Int)
-rowBounds s = (\((i,_),(j,_)) -> (i, j)) $ bounds (us s)
+colorBounds :: State -> (Int, Int)
+colorBounds s = (\((_,i),(_,j)) -> (i, j)) $ bounds (us s)
 
-colBounds :: State -> (Int, Int)
-colBounds s = (\((_,i),(_,j)) -> (i, j)) $ bounds (us s)
+nodeIxs :: State -> [Int]
+nodeIxs = range . nodeBounds
 
-rowIxs :: State -> [Int]
-rowIxs = range . rowBounds
+colorIxs :: State -> [Int]
+colorIxs = range . colorBounds
 
-colIxs :: State -> [Int]
-colIxs = range . colBounds
+nodeVs :: State -> Int -> [Int]
+nodeVs s n = [vs s ! (n, c) | c <- colorIxs s]
 
-vertexVs :: State -> Int -> [Int]
-vertexVs s v = [vs s ! (v, c) | c <- colIxs s]
-
-colorVs s c = [vs s ! (v, c) | v <- rowIxs s]
+colorVs s c = [vs s ! (n, c) | n <- nodeIxs s]
 
 a = 10
 b = 1
 c = 20
 
-dudt :: Graph -> State -> (Int, Int) -> Float
-dudt g s (vertex, color) = -(us s ! (vertex, color)) - fromIntegral
+dt = 0.01
+
+dudt :: UGraph -> State -> (Int, Int) -> Float
+dudt g s (node, color) = -(us s ! (node, color)) - fromIntegral
   (a * oneColorPerVertex + b * minimizeColor + c * noSameColorConnected)
-  where oneColorPerVertex = sum (vertexVs s vertex) - 1
-        minimizeColor = color - (fst $ colBounds s)
+  where oneColorPerVertex = sum (nodeVs s node) - 1
+        minimizeColor = color - (fst $ colorBounds s)
         noSameColorConnected =
-          sum [cv g vertex j * (vs s ! (j, color)) | j <- rowIxs s] - 1
+          sum [cv g node j * (vs s ! (j, color)) | j <- nodeIxs s] - 1
 
 --iterate :: State -> State
 --iterate s = Map.mapWithKey f 
